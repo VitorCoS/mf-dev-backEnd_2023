ac6d292092602e39a0d29fe5791f70f87ce5c236
Vitor Oliveira <vitor_cos12@yahoo.com.br>
2023-09-23 23:01:36 -0300

    first database config

--- mf-dev-backEnd_2023/Program.cs
+++ mf-dev-backEnd_2023/Program.cs
@@ -1,3 +1,6 @@
+using mf_dev_backEnd_2023.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace mf_dev_backEnd_2023
 {
     public class Program
@@ -9,6 +12,8 @@ namespace mf_dev_backEnd_2023
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+            builder.Services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
